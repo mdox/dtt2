8f6beecbde49f956621677d98d8b946196f3e3e6
Marton <mdox@mdox.xyz>
2022-09-03 19:49:23 +0200

    Client: Fetch drivers.

--- client/src/pages/Drivers.tsx
+++ client/src/pages/Drivers.tsx
@@ -1,7 +1,21 @@
+import { useMemo } from "react";
 import { Link } from "react-router-dom";
+import useSWR from "swr";
 import { DriverCard } from "../components/DriverCard";
+import { Driver } from "../lib/types";
 
 export function Drivers() {
+  // SWR
+  const { data } = useSWR<Driver[]>("/api/drivers", (url) =>
+    fetch(url).then((res) => res.json())
+  );
+
+  // Memos
+  const memoDrivers = useMemo(() => {
+    if (!data) return [];
+    return data.sort((a, b) => (a.place! < b.place! ? -1 : 1));
+  }, [data]);
+
   // Renders
   return (
     <div className="flex flex-col gap-2">
@@ -9,16 +23,11 @@ export function Drivers() {
         <Link to="/">Back to Home please.</Link>
       </p>
 
-      <DriverCard
-        id={-23}
-        firstname="John"
-        lastname="Doe"
-        code="CODE_231"
-        country="FR"
-        team="Red"
-        imgUrl="/static/alb.png"
-        place={3}
-      ></DriverCard>
+      <div className="flex flex-col gap-2">
+        {memoDrivers.map((driver) => (
+          <DriverCard key={driver.id} {...driver} />
+        ))}
+      </div>
     </div>
   );
 }
