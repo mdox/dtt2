71383f014613dd76e1f334963dda2d75430e4904
Marton <mdox@mdox.xyz>
2022-09-03 17:08:43 +0200

    Connect Client and Server to eachother.

--- server/src/index.ts
+++ server/src/index.ts
@@ -6,6 +6,8 @@ const app = express();
 
 app.use(express.json());
 
+app.get("/test", (req, res) => res.status(200).send("Hi!"));
+
 app.listen(PORT, () => {
   console.log(`Server is up on PORT: ${PORT}`);
 });
