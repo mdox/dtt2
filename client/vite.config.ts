80dedcdcc8c91a8a0c48c8af30c155c05d1d4042
Marton <mdox@mdox.xyz>
2022-09-03 18:57:37 +0200

    Client: Static port.

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from "vite";
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
+    port: 3000,
     proxy: {
       "/api": {
         target: "http://localhost:5000",
